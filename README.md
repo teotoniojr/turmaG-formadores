bebbdbd20765ea8e0f121ac9a890890bfc084b32
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2023-10-11 09:24:27 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# turmaG-formadores
+# EfeitosSonorosRadioGremio
+Mesa de mídias com áudios para efeitos sonoros especiais da rádio Grêmio Estudantil
---
+++ main.js
@@ -0,0 +1,5 @@
+function tocaSomGrilo(){
+    document.querySelector('#som_tecla_grilo').play();
+}
+
+document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
